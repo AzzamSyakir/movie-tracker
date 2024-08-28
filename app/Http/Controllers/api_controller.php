ae38f82c3a030330b859b3b4a65eabea58f68dc4
Azzam Syakir <azzamsykir@gmail.com>
2024-08-28 10:57:03 +0700

    feat: add function for TMDb API integration

--- app/Http/Controllers/api_controller.php
+++ app/Http/Controllers/api_controller.php
@@ -13,7 +13,7 @@ class api_controller
     {
         $this->apiKey = env('TMDB_API_KEY');
     }
-
+// movie list
     public function getPopularMovies()
     {
         try {
@@ -30,7 +30,52 @@ class api_controller
             throw new \Exception('Failed to fetch popular movies: ' . $e->getMessage());
         }
     }
-    
+    public function GetTopRatedMovies(){
+        try {
+            $response = Http::get("https://api.themoviedb.org/3/movie/top_rated", [
+                'api_key' => $this->apiKey
+            ]);
+
+            if ($response->successful()) {
+                return $response->json();
+            } else {
+                throw new \Exception('API response error with status code: ' . $response->status());
+            }
+        } catch (\Exception $e) {
+            throw new \Exception('Failed to fetch movie details: ' . $e->getMessage());
+        }
+    }
+    public function GetNowPlayingMovies(){
+        try {
+            $response = Http::get("https://api.themoviedb.org/3/movie/now_playing", [
+                'api_key' => $this->apiKey
+            ]);
+
+            if ($response->successful()) {
+                return $response->json();
+            } else {
+                throw new \Exception('API response error with status code: ' . $response->status());
+            }
+        } catch (\Exception $e) {
+            throw new \Exception('Failed to fetch movie details: ' . $e->getMessage());
+        }
+    }
+    public function GetUpcomingMovies(){
+        try {
+            $response = Http::get("https://api.themoviedb.org/3/movie/upcoming", [
+                'api_key' => $this->apiKey
+            ]);
+
+            if ($response->successful()) {
+                return $response->json();
+            } else {
+                throw new \Exception('API response error with status code: ' . $response->status());
+            }
+        } catch (\Exception $e) {
+            throw new \Exception('Failed to fetch movie details: ' . $e->getMessage());
+        }
+    }
+// movies 
     public function getMovieDetails($id)
     {
         try {
@@ -47,8 +92,22 @@ class api_controller
             throw new \Exception('Failed to fetch movie details: ' . $e->getMessage());
         }
     }
-    
+    public function SearchMovie($query){
+        try{
+        $response = Http::get("https://api.themoviedb.org/3/search/movie", [
+            'querry' => $query
+        ]);
 
+        if ($response->successful()) {
+            return $response->json();
+        } else {
+            throw new \Exception('API response error with status code: ' . $response->status());
+        }
+    }
+    catch (\Exception $e) {
+        throw new \Exception('Failed to fetch movie details: ' . $e->getMessage());
+    }
+    }
     
 }
 
