2e17c73069ec48f9b2f7ffed27f6619ec0387c4c
Azzam Syakir <azzamsykir@gmail.com>
2024-09-04 13:35:26 +0700

    refactor code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,23 @@
 namespace App\Http\Controllers;
 class HomeController
 {
+    protected $apiController;
+    public function __construct()
+    {
+        $this->apiController = new ApiController();
+    }
     public function homePage()
     {
-        $apiController = new ApiController();
-        $popularMovies = $apiController->getPopularMovies();
-        $nowPlayingMovies = $apiController->GetNowPlayingMovies();
-        $topRatedMovies = $apiController->GetTopRatedMovies();
-        // dd($apiController, $popularMovies, $nowPlayingMovies, $topRatedMovies);
+        $popularMovies = $this->apiController->getPopularMovies();
+        $nowPlayingMovies = $this->apiController->GetNowPlayingMovies();
+        $topRatedMovies = $this->apiController->GetTopRatedMovies();
+        // dd($popularMovies, $nowPlayingMovies, $topRatedMovies);
         return view('home', compact('popularMovies', 'nowPlayingMovies', 'topRatedMovies'));
+    }
+    public function movieDetail($movieId)
+    {
+        $searchMovie = $this->apiController->SearchMovie($movieId);
+        dd($movieId);
     }
     
 }
