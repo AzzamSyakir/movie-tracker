12adab25c1addd964bccb26bca8d92b0ccc46c80
Azzam Syakir <azzamsykir@gmail.com>
2024-09-04 13:35:40 +0700

    feat: add routes for movie-detail

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'homePage'])->name('homePage');
+Route::get('/movie-details/{movieId}', [HomeController::class, 'movieDetail'])->name('movieDetail');
