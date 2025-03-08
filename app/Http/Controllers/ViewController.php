49a7bb5520f387259899141f692845cb2b2c40fe
Azzamsyakir <azzamsykir@gmail.com>
2025-03-08 13:51:15 +0700

    fix: remove filterMovieByMpaa

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -18,9 +18,10 @@ class ViewController
     {
         $this->apiController = new ApiController();
     }
-    public function homepage() {
+    public function homepage()
+    {
         $client = new Client(['base_uri' => 'http://nginx/']);
-    
+
         try {
             $popularMoviesResponse = $client->get('popular-movies');
             if ($popularMoviesResponse->getStatusCode() === 200) {
@@ -29,7 +30,7 @@ class ViewController
         } catch (\Exception $e) {
             throw new \Exception('Failed to fetch popular movies: ' . $e->getMessage());
         }
-    
+
         try {
             $nowPlayingMoviesResponse = $client->get('nowPlaying-movies');
             if ($nowPlayingMoviesResponse->getStatusCode() === 200) {
@@ -38,7 +39,7 @@ class ViewController
         } catch (\Exception $e) {
             throw new \Exception('Failed to fetch now playing movies: ' . $e->getMessage());
         }
-    
+
         try {
             $topRatedMoviesResponse = $client->get('topRated-movies');
             if ($topRatedMoviesResponse->getStatusCode() === 200) {
@@ -47,40 +48,40 @@ class ViewController
         } catch (\Exception $e) {
             throw new \Exception('Failed to fetch top rated movies: ' . $e->getMessage());
         }
-    
+
         if (Auth::check()) {
             $userAuthenticate = true;
             return view('Home', compact('popularMovies', 'nowPlayingMovies', 'topRatedMovies', 'userAuthenticate'));
         }
-    
+
         return view('Home', compact('popularMovies', 'nowPlayingMovies', 'topRatedMovies'));
     }
-    
+
     public function movieDetail($movieId)
     {
         $movieVideos = $this->apiController->getMovieVideos($movieId);
         $movieDetails = $this->apiController->getMovieDetails($movieId);
-    
+
         if ($movieDetails !== null) {
             return view('MovieDetail', compact('movieDetails', 'movieVideos'));
         }
-    
+
         return view('MovieDetail', compact('movieVideos'));
     }
     public function GetWatchlist()
     {
         $userId = Auth::user()->id;
         $watchlist = Watchlist::where('user_id', $userId)->first();
-    
+
         $watchlistArray = [
             'name' => Auth::user()->name,
         ];
-    
+
         $createdSince = Carbon::parse($watchlist->created_at);
         $today = Carbon::now();
         $daysSinceCreation = $createdSince->diffInDays($today);
         $daysSinceCreation = round($daysSinceCreation);
-    
+
         if ($daysSinceCreation >= 365) {
             $yearsSinceCreation = round($daysSinceCreation / 365);
             $watchlistArray['createdSince'] = $yearsSinceCreation . ' year' . ($yearsSinceCreation > 1 ? 's' : '') . ' ago';
@@ -96,7 +97,7 @@ class ViewController
             $watchlistArray['createdSince'] = 'today';
         }
         $movies = $watchlist->watchlistMovies;
-    
+
         if ($movies->isNotEmpty()) {
             foreach ($movies as $watchlistMovie) {
                 $movieId = $watchlistMovie->movie_id;
@@ -105,52 +106,57 @@ class ViewController
             }
             return view('Watchlist', compact('watchlistArray'));
         }
-        
+
         return view('Watchlist', compact('watchlistArray'))->with('message', $movies->isEmpty() ? 'this list is empty.' : '');
     }
-    
-    public function addWatchlist($movieId) {
+
+    public function addWatchlist($movieId)
+    {
         $userId = Auth::user()->id;
         $watchlist = Watchlist::where('user_id', $userId)->first();
         $watchlistMovies = new WatchlistMovie([
-            'id' =>  Str::uuid(),
+            'id' => Str::uuid(),
             'movie_id' => $movieId,
             'watchlist_id' => $watchlist->id
         ]);
         $watchlistMovies->save();
         return redirect()->back();
-        }
-    public function DeleteWatchlist($movieId){
-        $WatchlistMovie = WatchlistMovie::where('movie_id',$movieId);
+    }
+    public function DeleteWatchlist($movieId)
+    {
+        $WatchlistMovie = WatchlistMovie::where('movie_id', $movieId);
         $WatchlistMovie->delete();
         return redirect()->back();
     }
-    public function SearchMovie($query) {
+    public function SearchMovie($query)
+    {
         $searchMovie = $this->apiController->SearchMovie($query);
         return response()->json($searchMovie);
     }
-    public function FindMovie($query) {
+    public function FindMovie($query)
+    {
         $searchMovie = $this->apiController->SearchMovie($query);
         if ($searchMovie == null) {
             $searchMovie = [];
-            return view('Find', compact('searchMovie'))->with('keyword',  $query);
+            return view('Find', compact('searchMovie'))->with('keyword', $query);
         }
-        return view('Find', compact('searchMovie'))->with('keyword', $query);   
+        return view('Find', compact('searchMovie'))->with('keyword', $query);
     }
-    public function updateUser(Request $request) {
+    public function updateUser(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'email' => ['nullable', 'email'],
             'password' => ['nullable', 'min:8'],
             'name' => ['nullable']
         ]);
-    
+
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 422);
         }
-    
+
         $auth = Auth::user();
         $findUser = User::find($auth->id);
-        
+
         if (array_key_exists('password', $request->all()) && array_key_exists('email', $request->all())) {
             if (!Hash::check($request->input('password'), $findUser->password)) {
                 return response()->json(['error' => 'Sorry, wrong password'], 422);
@@ -158,21 +164,22 @@ class ViewController
             $auth->password = Hash::make($request->input('password'));
             $auth->email = $request->input('email');
         }
-        
+
         if ($request->input('name') !== null) {
             $auth->name = $request->input('name');
         }
-    
+
         $auth->save();
-    
+
         return response()->json(['success' => 'Account updated successfully']);
     }
-    
-    public function AccountSettingView() {
+
+    public function AccountSettingView()
+    {
         $auth = Auth::user();
-        $user =  [
+        $user = [
             'name' => $auth->name,
-            'email' =>$auth->email,
+            'email' => $auth->email,
         ];
         if ($auth->oauth_provider == 'local') {
             return view('AccountSetting', compact('user'))->with('oauthProvider', 'local');
